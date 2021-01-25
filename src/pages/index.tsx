46f7f5a2f32eeb554679d78875d73b09ea5209a4
Jade <e2goon@gmail.com>
2021-01-26 02:20:11 +0900

    refactor: classify uuid as utils

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,20 +10,7 @@ import { useRecoilState } from 'recoil'
 import Layout from '@/layouts/BasicLayout'
 import Checkbox from '@/components/Checkbox'
 import { todosState } from '@/states'
-
-const uuid = (): string => {
-  let i = 0
-  let random = null
-  let uuid = ''
-  for (i; i < 32; i++) {
-    random = (Math.random() * 16) | 0
-    if (i === 8 || i === 12 || i === 16 || i === 20) {
-      uuid += '-'
-    }
-    uuid += (i === 12 ? 4 : i === 16 ? (random & 3) | 8 : random).toString(16)
-  }
-  return uuid
-}
+import { uuid } from '@/utils'
 
 const Tasks: NextPage = () => {
   const textInput = createRef<HTMLInputElement>()
---
+++ src/utils/index.ts
@@ -0,0 +1,15 @@
+const uuid = (): string => {
+  let i = 0
+  let random = null
+  let uuid = ''
+  for (i; i < 32; i++) {
+    random = (Math.random() * 16) | 0
+    if (i === 8 || i === 12 || i === 16 || i === 20) {
+      uuid += '-'
+    }
+    uuid += (i === 12 ? 4 : i === 16 ? (random & 3) | 8 : random).toString(16)
+  }
+  return uuid
+}
+
+export { uuid }
