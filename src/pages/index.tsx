1f57c997b35645abc79269de296d4b21ba075b34
Jade <e2goon@gmail.com>
2021-01-26 03:57:01 +0900

    refactor: re-render issue - 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import {
   useState,
   createRef,
+  useCallback,
+  memo,
   SyntheticEvent,
   FormEvent,
+  FC,
 } from 'react'
 import { NextPage } from 'next'
 import Link from 'next/link'
@@ -17,54 +20,45 @@ const Tasks: NextPage = () => {
   const [input, setInput] = useState('')
   const [todos, setTodos] = useRecoilState(todosState)
 
-  const handleChange = (e: FormEvent<HTMLInputElement>) =>
-    setInput(e.currentTarget.value)
+  const handleChange = useCallback(
+    (e: FormEvent<HTMLInputElement>) => setInput(e.currentTarget.value),
+    [input]
+  )
 
-  const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault()
+  const handleSubmit = useCallback(
+    (e: SyntheticEvent) => {
+      e.preventDefault()
 
-    if (input.length <= 0) return
+      if (input.length <= 0) return
 
-    const todo = {
-      id: uuid(),
-      name: input,
-      checked: false,
-    }
-    setTodos([todo, ...todos])
-    setInput('')
-    textInput.current.focus()
-  }
+      const todo = {
+        id: uuid(),
+        name: input,
+        checked: false,
+      }
+      setTodos([todo, ...todos])
+      setInput('')
+      textInput.current.focus()
+    },
+    [input, todos]
+  )
 
-  const handleToggle = (todo) => () => {
-    const newTodos = todos.map((item) => {
-      return item !== todo
-        ? item
-        : { ...item, ...{ checked: !item.checked } }
-    })
-    setTodos(newTodos)
-  }
+  const handleToggle = useCallback(
+    (todo) => () => {
+      const newTodos = todos.map((item) => {
+        return item !== todo ? item : { ...item, ...{ checked: !item.checked } }
+      })
+      setTodos(newTodos)
+    },
+    [todos]
+  )
 
-  const renderFilteredTodos = (isChecked: boolean) => {
-    const filteredTodos = todos.filter((todo) => todo.checked === isChecked)
-    return filteredTodos.map((todo, index) => (
-      <div key={index} role="listitem" className="my-4 relative">
-        <Link href={`/${todo.id}`}>
-          <a className="block py-4 px-6 w-full bg-white rounded-2xl outline-none hover:underline focus:ring-2 focus:ring-gray-600">
-            {todo.name}
-          </a>
-        </Link>
-        <Checkbox
-          onChange={handleToggle(todo)}
-          checked={todo.checked}
-          className="absolute top-1/2 right-4 transform-gpu -translate-y-1/2"
-        />
-      </div>
-    ))
-  }
-
-  return (
-    <Layout>
-      <form onSubmit={handleSubmit} className="my-10">
+  const CreateTodo: FC<{
+    onSubmit: (e: SyntheticEvent) => void
+    onChange: (e: FormEvent<HTMLInputElement>) => void
+  }> = memo(function CreateTodo({ onSubmit, onChange }) {
+    return (
+      <form onSubmit={onSubmit} className="my-10">
         <fieldset className="flex">
           <legend className="sr-only">New Task</legend>
           <label className="flex-1 mr-2">
@@ -74,29 +68,58 @@ const Tasks: NextPage = () => {
               className="block py-4 px-6 w-full bg-white rounded-2xl outline-none ring-2 ring-transparent hover:ring-gray-300 focus:ring-gray-600"
               placeholder="Write a new task"
               value={input}
-              onChange={handleChange}
+              onChange={onChange}
               autoFocus={true}
             />
           </label>
           <button
             type="button"
             className="py-4 px-6 bg-gray-900 text-white rounded-2xl focus:outline-none"
-            onClick={handleSubmit}
+            onClick={onSubmit}
           >
             Add
           </button>
         </fieldset>
       </form>
+    )
+  })
 
-      <section className="my-10">
-        <h2 className="mb-4 text-lg font-bold">Todos</h2>
-        <div role="list">{renderFilteredTodos(false)}</div>
-      </section>
-
+  const TodoList: FC<{
+    title: string
+    todos
+    isChecked?: boolean
+    onToggle: (todo: any) => () => void
+  }> = memo(function TodoList({ title, todos, isChecked = false, onToggle }) {
+    const filteredTodos = todos.filter((todo) => todo.checked === isChecked)
+    return (
       <section>
-        <h2 className="mb-4 text-lg font-bold">Done</h2>
-        <div role="list">{renderFilteredTodos(true)}</div>
+        <h2 className="mb-4 text-lg font-bold">{title}</h2>
+        <div role="list">
+          {filteredTodos.map((todo, index) => (
+            <div key={index} role="listitem" className="my-4 relative">
+              <Link href={`/${todo.id}`}>
+                <a className="block py-4 px-6 pr-12 w-full bg-white rounded-2xl outline-none truncate hover:underline focus:ring-2 focus:ring-gray-600">
+                  {todo.name}
+                </a>
+              </Link>
+              <Checkbox
+                onChange={onToggle(todo)}
+                checked={todo.checked}
+                className="absolute top-1/2 right-4 transform-gpu -translate-y-1/2"
+              />
+            </div>
+          ))}
+        </div>
       </section>
+    )
+  })
+
+  return (
+    <Layout>
+      <CreateTodo onChange={handleChange} onSubmit={handleSubmit} />
+
+      <TodoList title="Todos" todos={todos} onToggle={handleToggle} />
+      <TodoList title="Done" todos={todos} onToggle={handleToggle} isChecked />
     </Layout>
   )
 }
